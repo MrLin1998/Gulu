3881cb046bb732e31c46941b29c2eb7c3a635f0a
linzb <1044034806@qq.com>
2024-08-07 10:42:53 +0800

    新增营销模块

---
+++ markert/src/main/java/com/linzb/gulumarket/GuluMarketApplication.java
@@ -0,0 +1,13 @@
+package com.linzb.gulumarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
+public class GuluMarketApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GuluMarketApplication.class,args);
+        System.out.println("=======启动成功=============");
+    }
+}
